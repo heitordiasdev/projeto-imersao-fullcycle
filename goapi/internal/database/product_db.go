a6414d970dabc3dba7621cab1f96255c58ab2932
heitordiasdev <heitordiastavares3@gmail.com>
2024-01-31 14:21:30 -0300

    fix: Correct parameters in product_db.go
    Fix the passing of parameters in the function handling GET requests in product_db.go. The error occurred due to a mismatch in the passed parameters, now adjusted to ensure correct functioning of GET requests.

--- goapi/internal/database/product_db.go
+++ goapi/internal/database/product_db.go
@@ -15,7 +15,7 @@ func NewProductDB(db *sql.DB) *ProductDB {
 }
 
 func (pd *ProductDB) GetProducts() ([]*entity.Product, error) {
-	rows, err := pd.db.Query("SELECT name, price, category_id FROM products")
+	rows, err := pd.db.Query("SELECT id, name, description, price, category_id, image_url FROM products")
 	if err != nil{
 		return nil, err
 	}
@@ -25,7 +25,7 @@ func (pd *ProductDB) GetProducts() ([]*entity.Product, error) {
 	var products []*entity.Product
 	for rows.Next() {
 		var product entity.Product
-		if err := rows.Scan(&product.ID, &product.Name, &product.Price, &product.CategoryID); err != nil{
+		if err := rows.Scan(&product.ID, &product.Name, &product.Description, &product.Price, &product.CategoryID, &product.ImageURL); err != nil{
 			return nil, err
 		}
 		products = append(products, &product)
@@ -33,7 +33,7 @@ func (pd *ProductDB) GetProducts() ([]*entity.Product, error) {
 	return products, nil
 }
 
-func (pd *ProductDB) GetProduct (id string) (*entity.Product, error) {
+func (pd *ProductDB) GetProduct(id string) (*entity.Product, error) {
 	var product entity.Product
 	err := pd.db.QueryRow("SELECT id, name, description, price, category_id, image_url FROM products WHERE id = ?", id).
 		Scan(&product.ID, &product.Name, &product.Description ,&product.Price, &product.CategoryID, &product.ImageURL)
@@ -43,7 +43,7 @@ func (pd *ProductDB) GetProduct (id string) (*entity.Product, error) {
 	return &product, nil
 }
 
-func (pd *ProductDB) GetProductByCategoryID (categoryID string) ([]*entity.Product, error) {
+func (pd *ProductDB) GetProductByCategoryID(categoryID string) ([]*entity.Product, error) {
 	rows, err := pd.db.Query("SELECT id, name, description, price, category_id, image_url FROM products where category_id = ?",categoryID)
 	if err != nil {
 		return nil, err
@@ -62,7 +62,7 @@ func (pd *ProductDB) GetProductByCategoryID (categoryID string) ([]*entity.Produ
 
 }
 
-func (pd *ProductDB) CreateProduct (product *entity.Product) (*entity.Product, error) {
+func (pd *ProductDB) CreateProduct(product *entity.Product) (*entity.Product, error) {
 	_, err := pd.db.Exec("INSERT INTO products (id, name, description, price, category_id, image_url) VALUES (?, ?, ?, ?, ?, ?)", 
 		product.ID, product.Name, product.Description, product.Price, product.CategoryID, product.ImageURL)
 	if err != nil {
